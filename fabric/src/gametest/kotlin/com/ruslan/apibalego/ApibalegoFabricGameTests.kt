ccd1ef9bdc4555bac3a9fefe66bf341c70bd61f1
filloax <filloax@gmail.com>
2026-07-13 21:57:19 +0200

    hopefully fix tests

--- fabric/src/gametest/kotlin/com/ruslan/apibalego/ApibalegoFabricGameTests.kt
+++ fabric/src/gametest/kotlin/com/ruslan/apibalego/ApibalegoFabricGameTests.kt
@@ -12,42 +12,9 @@ import net.minecraft.gametest.framework.GameTestHelper
  * Run with: `./gradlew :fabric:runGametest` (see build.gradle.kts).
  */
 object ApibalegoFabricGameTests {
-    @GameTest
-    fun configLoaded(helper: GameTestHelper) = ApibalegoGameTests.configLoaded(helper)
-
-    @GameTest
-    fun apiEventDispatchActive(helper: GameTestHelper) = ApibalegoGameTests.apiEventDispatchActive(helper)
-
-    @GameTest
-    fun apiEventDispatchHitsAbsentType(helper: GameTestHelper) = ApibalegoGameTests.apiEventDispatchHitsAbsentType(helper)
-
-    @GameTest
-    fun liveUpdatesBuiltinsRegistered(helper: GameTestHelper) = ApibalegoGameTests.liveUpdatesBuiltinsRegistered(helper)
-
-    @GameTest
-    fun dispatchJoinActive(helper: GameTestHelper) = ApibalegoGameTests.dispatchJoinActive(helper)
-
-    @GameTest
-    fun dispatchJoinInactiveSkipped(helper: GameTestHelper) = ApibalegoGameTests.dispatchJoinInactiveSkipped(helper)
-
-    @GameTest
-    fun toastJoinMarksAsSeen(helper: GameTestHelper) = ApibalegoGameTests.toastJoinMarksAsSeen(helper)
-
-    @GameTest
-    fun commandDispatchRunsCommand(helper: GameTestHelper) = ApibalegoGameTests.commandDispatchRunsCommand(helper)
-
-    @GameTest
-    fun commandDispatchIdempotent(helper: GameTestHelper) = ApibalegoGameTests.commandDispatchIdempotent(helper)
-
-    @GameTest
-    fun structureHandlerGeneralTest(helper: GameTestHelper) = ApibalegoGameTests.structureHandlerGeneralTest(helper)
-
-    @GameTest
-    fun datapackHandlerSkipsWhenDisabled(helper: GameTestHelper) = ApibalegoGameTests.datapackHandlerSkipsWhenDisabled(helper)
-
-    // generous budget: this test chains 4 sequential download+reload+select round trips
-    // (install, version bump, URL-only change, removal), which can run long under a loaded
-    // gametest server ("Can't keep up!" throttling)
-    @GameTest(maxTicks = 1000)
-    fun datapackHandlerFullLifecycle(helper: GameTestHelper) = ApibalegoGameTests.datapackHandlerFullLifecycle(helper)
+    // generous budget: chains config/join/command/structure/datapack checks as one continuous
+    // sequence (see ApibalegoGameTests.combinedHandlerLifecycleTest), which can run long under a
+    // loaded gametest server ("Can't keep up!" throttling)
+    @GameTest(maxTicks = 1200)
+    fun combinedHandlerLifecycleTest(helper: GameTestHelper) = ApibalegoGameTests.combinedHandlerLifecycleTest(helper)
 }
